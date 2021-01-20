9ba00db87e43b6992408fdea52fae63755bb6342
Pieter Timmerman <p.h.timmerman@st.hanze.nl>
2021-01-20 14:47:28 +0100

    indent

--- Weerstation/inloggen.php
+++ Weerstation/inloggen.php
@@ -1,31 +1,32 @@
 <?php session_start(); ?>
-    <html>
+<html>
     <head>
         <title>Log in</title>
     </head>
     <body>
-<?php
-$gebruikersnaam = $_POST['user'];
-$wachtwoord = $_POST['password'];
+        <?php
+            $gebruikersnaam = $_POST['user'];
+            $wachtwoord = $_POST['password'];
 
-require('database.lib.php');
-$dbConnection = dbConnect();
-$sql = "SELECT * FROM admin
-     WHERE user = '$gebruikersnaam'
-     AND password = '$wachtwoord'";
-$result = mysqli_query($dbConnection,$sql);
-if($result->num_rows > 0){
-    $row = $result->fetch_assoc();
-    $_SESSION['persoon'] = 1;
-    echo "<script>
-          alert('succesvol ingelogd, klik op OK om te bestellen');
-          window.location.href='index.php';
-         </script>";
-}else {
-    echo "<script>
-          alert('verkeerde wachtwoord of gebruikersnaam, probeer het opnieuw');
-          window.location.href='login.php';
-         </script>";
-}
-
-?>
+            require('database.lib.php');
+            $dbConnection = dbConnect();
+            $sql = "SELECT * FROM admin
+                    WHERE user = '$gebruikersnaam'
+                    AND password = '$wachtwoord'";
+            $result = mysqli_query($dbConnection,$sql);
+            if($result->num_rows > 0){
+                $row = $result->fetch_assoc();
+                $_SESSION['persoon'] = 1;
+                echo "<script>
+                      alert('succesvol ingelogd, klik op OK om te bestellen');
+                      window.location.href='index.php';
+                     </script>";
+            }else {
+                echo "<script>
+                      alert('verkeerde wachtwoord of gebruikersnaam, probeer het opnieuw');
+                      window.location.href='login.php';
+                     </script>";
+            }
+        ?>
+    </body>
+</html>
--- Weerstation/login.php
+++ Weerstation/login.php
@@ -1,29 +1,27 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Aitken Spence Login</title>
-    <link rel="stylesheet" type="text/css" href="login.css">
-</head>
-<body>
-<div class="header">
-    <h2>Login</h2>
-
-</div>
-
-<form name="formulier" method="post" action="inloggen.php">
-    <a class='button1' href='login.php'><img src="pictures/logo.png" width="100%"></a>
-    <div class="input-group">
-        <label>User</label>
-        <input type="text" name="user" >
-    </div>
-    <div class="input-group">
-        <label>Password</label>
-        <input type="text" name="password">
-    </div>
-    <div class="input-group">
-        <button type="submit" class="submitButton" name="bestel">Log in</button>
-    </div>
-</form>
-</body>
+    <head>
+        <title>Aitken Spence Login</title>
+        <link rel="stylesheet" type="text/css" href="login.css">
+    </head>
+    <body>
+        <div class="header">
+            <h2>Login</h2>
+        </div>
+        <form name="formulier" method="post" action="inloggen.php">
+            <a class='button1' href='login.php'><img src="pictures/logo.png" width="100%"></a>
+            <div class="input-group">
+                <label>User</label>
+                <input type="text" name="user" >
+            </div>
+            <div class="input-group">
+                <label>Password</label>
+                <input type="text" name="password">
+            </div>
+            <div class="input-group">
+                <button type="submit" class="submitButton" name="bestel">Log in</button>
+            </div>
+        </form>
+    </body>
 </html>
