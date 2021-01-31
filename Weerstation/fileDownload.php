a72b04d7b2189a5955558ec6efe958a8dc8647c8
Pieter Timmerman <p.h.timmerman@st.hanze.nl>
2021-01-31 22:02:44 +0100

    XML download

--- Weerstation/fileDownload.php
+++ Weerstation/fileDownload.php
@@ -7,23 +7,23 @@
             $strJsonFileContents = file_get_contents("data/" . $file);
             $arrayJson = json_decode($strJsonFileContents, true);
 
-            header('Content-type: text/xml');
+            header('Content-type: application/octet-stream');
             header('Content-Disposition: attachment; filename="' . $file . '.xml"');
 
-            echo assocArrayToXML("weather_data", $arrayJson);
+            echo assocArrayToXML("root", $arrayJson);
             exit();
         }
     }
     function assocArrayToXML($root_element_name,$ar)
     {
-        $xml = new SimpleXMLElement("<?xml ?><{$root_element_name}></{$root_element_name}>");
+        $xml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"UTF-8\"?><{$root_element_name}></{$root_element_name}>");
         $f = function($f,$c,$array) {
             foreach($array as $key=>$value) {
                 if(is_array($value)) {
-                    $ch=$c->addChild($key);
+                    $ch=$c->addChild("weatherdata");
                     $f($f,$ch,$value);
                 } else {
-                    $c->addChild($key,$value);
+                    $c->addChild($key, $value);
                 }
             }
         };
